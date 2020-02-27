3a0d50472046ffca47e2779d07dadae80daa65d5
Derrick Luyen <46693824+derrickluyen@users.noreply.github.com>
2020-02-27 12:21:35 -1000

    fixed mistake where we forgot end:vevent

--- calendar.js
+++ calendar.js
@@ -3,12 +3,13 @@ function submitForm() {
   VERSION:2.0\r\n
   CALSCALE:GREGORIAN\r\n
   BEGIN:VEVENT\r\n
-  DTSTART:20200313T010000Z\r\n
-  DTEND:20200313T033000\r\n
+  DTSTART:20200313T200000Z\r\n
+  DTEND:20200313T230000Z\r\n
   DTSTAMP:20200227T233911Z\r\n
   SUMMARY:Study Session\r\n
   DESCRIPTION:Study for Exam\r\n
   LOCATION:Hamilton Library\r\n
+  END:VEVENT\r\n
   END:VCALENDAR`;
   const file = new Blob([data], { type: 'text/plain;charset=utf-8' });
   saveAs(file, `study.ics`);
