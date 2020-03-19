9db5efed73753afaac38408001c5a56d59896212
Derrick Luyen <46693824+derrickluyen@users.noreply.github.com>
2020-03-19 11:45:58 -1000

    added regex for location, added form no refresh

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Sites referenced:
 https://github.com/eligrey/FileSaver.js/
 https://stackoverflow.com/questions/21997057/how-to-use-filesaver-js
 https://github.com/TeamRambutan/Calendar
+https://www.w3resource.com/javascript/form/all-letters-field.php
+https://stackoverflow.com/questions/576196/regular-expression-allow-letters-numbers-and-spaces-with-at-least-one-letter
--- calendar.js
+++ calendar.js
@@ -1,36 +1,50 @@
 function submitForm() {
-  const summary = document.getElementById("summary").value;
+  let summary = document.getElementById("summary").value;
   if (!summary) {
     alert("The summary field is required");
     return;
   }
 
-  const location = document.getElementById("location").value;
+  let location = document.getElementById("location").value;
   if (!location) {
     alert("The location field is required");
     return;
   }
 
-  const description = document.getElementById("description").value;
-
-  const fname = document.getElementById("fname").value;
+  let fname = document.getElementById("fname").value;
   if (!fname) {
     alert("The event name field is required");
     return;
   }
 
-  const data = `BEGIN:VCALENDAR\r\n
-  VERSION:2.0\r\n
-  CALSCALE:GREGORIAN\r\n
-  BEGIN:VEVENT\r\n
-  DTSTART:20200313T200000Z\r\n
-  DTEND:20200313T230000Z\r\n
-  DTSTAMP:20200227T233911Z\r\n
-  SUMMARY:${summary}\r\n
-  DESCRIPTION:${description}\r\n
-  LOCATION:${location}\r\n
-  END:VEVENT\r\n
-  END:VCALENDAR`;
-  const file = new Blob([data], { type: 'text/plain;charset=utf-8' });
-  saveAs(file, `${fname}.ics`);
+  if (!(summary === '') && !(location === '') && !(fname === '') && validLoc(location)) {
+    let data = `BEGIN:VCALENDAR\r\n
+    VERSION:2.0\r\n
+    CALSCALE:GREGORIAN\r\n
+    BEGIN:VEVENT\r\n
+    DTSTART:20200313T200000Z\r\n
+    DTEND:20200313T230000Z\r\n
+    DTSTAMP:20200227T233911Z\r\n
+    SUMMARY:${summary}\r\n
+    DESCRIPTION:${document.getElementById("description").value}\r\n
+    LOCATION:${location}\r\n
+    END:VEVENT\r\n
+    END:VCALENDAR`;
+    let file = new Blob([data], { type: 'text/plain;charset=utf-8' });
+    saveAs(file, `${fname}.ics`);
+  }
+
+}
+
+function validLoc(input)
+{
+  let valid = /^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$/;
+  if(!input.match(valid))
+  {
+    alert('Please input letters, numbers, and spaces only in the LOCATION field');
+    return false;
+  }
+  else {
+    return true;
+  }
 }
