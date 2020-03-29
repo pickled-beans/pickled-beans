bc26a26ce5fb4c72becb2072494c90eeb4414840
Joseph Paragas <josephparagas5@gmail.com>
2020-03-29 13:11:20 -1000

    added time/date fields and also updated javascript file

--- calendar.js
+++ calendar.js
@@ -23,7 +23,26 @@ function submitForm() {
       return;
     }
 
-  const data = `BEGIN:VCALENDAR\r\n
+  const start = document.getElementById("dateStart").value;
+  const end = document.getElementById("dateEnd").value;
+
+  if(end === start) {
+    const startTime = document.getElementById("start-time").value;
+    const endTime = document.getElementById("end-time").value;
+    if (endTime >= startTime) {
+      createFile();
+    }
+    else {
+      alert("Error: End time should not be before start time");
+      console.assert((end === start) && (endTime < startTime), 'Invalid alert');
+    }
+  }
+  else {
+    alert("Error: End date should not be before start date");
+    console.assert(end < start, 'Invalid alert');
+  }
+
+  /*const data = `BEGIN:VCALENDAR\r\n
   VERSION:2.0\r\n
   CALSCALE:GREGORIAN\r\n
   BEGIN:VEVENT\r\n
@@ -36,5 +55,19 @@ function submitForm() {
   END:VEVENT\r\n
   END:VCALENDAR`;
   const file = new Blob([data], { type: 'text/plain;charset=utf-8' });
-  saveAs(file, `${ename}.ics`);
+  saveAs(file, `${ename}.ics`); */
+}
+
+//creates a new .ics file
+function createFile() {
+  let version = document.getElementById('version').value;
+  const data = `BEGIN:VCALENDAR\r\nVERSION:${version}\r\nCALSCALE:GREGORIAN\r\n${createVevent()}END:VCALENDAR`;
+  if (version === '1.0') {
+    const file = new Blob([data], { type: 'text/plain;charset=utf-8' });
+    saveAs(file, `${document.getElementById('summary').value}.vcs`);
+  }
+  else {
+    const file = new Blob([data], { type: 'text/plain;charset=utf-8' });
+    saveAs(file, `${document.getElementById('summary').value}.ics`);
+  }
 }
